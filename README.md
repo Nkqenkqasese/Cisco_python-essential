c736fbf063992c2a53fa9f9c7caea5fabb867d61
hrandrIAga <haga.randrianaly@gmail.com>
2022-10-07 00:46:15 +0200

    main requirements of TTT - no winner yet

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Cisco_python-essential
-Dev basic projects inspired by "Cisco : Python essential 1" &amp; "Cisco : Python essential 2" MOOCs
+#
+
+I started learning Python basics with the online MOOCs :
+	"Cisco: Python essential 1" & "Cisco: Python essential 2"
+	
+All the projects related to those Mooc are in this repository
--- Tic-tac-toe_project/Tic-tac-toe.py
+++ Tic-tac-toe_project/Tic-tac-toe.py
@@ -8,26 +8,104 @@
 #the board's appearance should be exactly the same as the one presented in the example.
 #implement the functions defined for you in the editor.
 
+from random import randrange
 
-def display_board(board):
-    # The function accepts one parameter containing the board's current status
-    # and prints it out to the console.
+row_1=[1,2,3]
+row_2=[4,'X',6]
+row_3=[7,8,9]
+empty_fields=[1,2,3,4,6,7,8,9]
 
+def display_board(line_1,line_2,line_3):
+    for i in range(4):
+        if i % 4 == 0:
+            print("+-------+-------+-------+")
+        elif i % 2 ==1:
+            print("|       |       |       |")
+        else:
+            print("|  ",line_1[i-2]," ","|  ",line_1[i-1]," ","|  ",line_1[i],"  |")
 
-def enter_move(board):
+    for i in range(4):
+        if i % 4 == 0:
+            print("+-------+-------+-------+")
+        elif i % 2 ==1:
+            print("|       |       |       |")
+        else:
+            print("|  ",line_2[i-2]," ","|  ",line_2[i-1]," ","|  ",line_2[i],"  |")
+
+    for i in range(5):
+        if i % 4 == 0:
+            print("+-------+-------+-------+")
+        elif i % 2 ==1:
+            print("|       |       |       |")
+        else:
+            print("|  ",line_3[i-2]," ","|  ",line_3[i-1]," ","|  ",line_3[i],"  |")
+
+def update_empty_fields(move):
+    for i in range(len(empty_fields)):
+      if empty_fields[i] == move:
+        j=i  
+        break 
+    del empty_fields[j]
+
+def enter_move(move):
     # The function accepts the board's current status, asks the user about their move, 
     # checks the input, and updates the board according to the user's decision.
+    print(move)
+    if move not in empty_fields:
+        print('impossible move')
+        move = input('Enter a valid move') 
+        enter_move(move) #ne fonctionne pas
+
+    if move < 4:
+        row_1[move-1]='0'
+        update_empty_fields(move)
+        
+    elif move > 3 and move < 7:
+        row_2[move-4]='0'
+        update_empty_fields(move)
+
+    elif move > 6:
+        row_3[move-7]='O'
+        update_empty_fields(move)
+
+    print(display_board(row_1,row_2,row_3))
+    
+def computer_move():
+    # The function enter and draws computer's random move
+    move=randrange(1,9,1)
+    print('The computer plays ', move)
+    if move not in empty_fields:
+        move=randrange(1,9,1)
+        computer_move()
+
+    elif move < 4:
+        row_1[move-1]='X'
+        update_empty_fields(move)
+        
+    elif move > 3 and move < 7:
+        row_2[move-4]='X'
+        update_empty_fields(move)
+
+    elif move > 6:
+        row_3[move-7]='X'
+        update_empty_fields(move)
+
+    print(display_board(row_1,row_2,row_3))
 
 
-def make_list_of_free_fields(board):
-    # The function browses the board and builds a list of all the free squares; 
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
-
-
-def victory_for(board, sign):
+#def victory_for(board, sign):
     # The function analyzes the board's status in order to check if 
     # the player using 'O's or 'X's has won the game
+    
 
 
-def draw_move(board):
-    # The function draws the computer's move and updates the board.
+###############MAIN########################
+print(display_board(row_1,row_2,row_3)) #initial board display
+for i in range(4):
+
+  move=int(input('Enter your move'))
+
+  enter_move(move)
+
+  computer_move()
+
